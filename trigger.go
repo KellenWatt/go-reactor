3f279af47877bafbbdfb86432d39b70743152246
Kellen Watt <kbw6d9@mst.edu>
2020-04-22 15:17:47 -0500

    Add documentation for Trigger

--- trigger.go
+++ trigger.go
@@ -4,6 +4,9 @@ import (
 	"sync"
 )
 
+// Trigger implements the Initiator interface. Trigger provides a mutex, Lock, 
+// as a convenience for handling shared resources in asynchronous and 
+// concurrent callbacks.
 type Trigger struct {
 	Lock sync.Mutex
 	value interface{}
@@ -14,6 +17,10 @@ type Trigger struct {
 	bindings []binding
 }
 
+// Value returns the value underlying t and runs any callbacks associated with 
+// reading. A type assertion will probably be needed to meaningfully use the 
+// returned value. If t has never been set, Value returns nil and any callbacks 
+// will be passed nil.
 func (t *Trigger) Value() interface{} {
 	t.Lock.Lock()
 		v := t.value
@@ -26,6 +33,9 @@ func (t *Trigger) Value() interface{} {
 	return v
 }
 
+// SetValue sets the value underlying t and runs any callbacks associated 
+// with writing. If the current value is nil (for example, if t has not been 
+// set yet), the previous value in callbacks will be nil.
 func (t *Trigger) SetValue(v interface{}) {
 	t.Lock.Lock()
 		prev := t.value
@@ -44,20 +54,32 @@ func (t *Trigger) SetValue(v interface{}) {
 	}
 }
 
-
+// AddBinder adds a Binder to be executed when the value of t changes. If 
+// concurrent is true, b.SetValue will not be called when t changes; instead it
+// will be queued for change.
+//
+// AddBinder is largely intended for use in implementing Binders, and its 
+// use is heavily discouraged outside of that. If concurrent is true outside 
+// of Binder implementations, f may or may not have the desired effect.
+// If concurrent is false, this will have exactly the same effect as 
+// Binder.AddBinding(), which is the preferred method of creating bindings.
 func (t *Trigger) AddBinder(b Binder, f BindingFunc, concurrent bool) {
 	t.bindings = append(t.bindings, binding{t, b, f, concurrent})
 }
 
-
+// AddReadCallback adds a callback that will be run when t is read using Value.
 func (t *Trigger) AddReadCallback(r ReadCallback) {
 	t.readCallbacks = append(t.readCallbacks, r)
 }
 
+// AddAsyncReadCallback adds a callback that will be run in a goroutine when t 
+// is read using Value.
 func (t *Trigger) AddAsyncReadCallback(r ReadCallback) {
 	t.readCallbacks = append(t.readCallbacks, makeAsyncRead(r))
 }
 
+// AddConcurrentReadCallback adds a callback that will be run concurrently
+// when t is read using Value.
 func (t *Trigger) AddConcurrentReadCallback(r ReadCallback) {
 	conReadLock.Lock()
 		if conRead == nil {
@@ -68,18 +90,26 @@ func (t *Trigger) AddConcurrentReadCallback(r ReadCallback) {
 	t.readCallbacks = append(t.readCallbacks, makeConcurrentRead(r))
 }
 
+// AddConditionalReadCallback adds a callback that will be run when t is read 
+// using Value only if f evaluates to true when passed the value of t.
 func (t *Trigger) AddConditionalReadCallback(r ReadCallback, f func(interface{})bool) {
 	t.readCallbacks = append(t.readCallbacks, makeConditionalRead(r, f))
 }
 
+// AddWriteCallback adds a callback that will be run when t is written to 
+// using SetValue.
 func (t *Trigger) AddWriteCallback(w WriteCallback) {
 	t.writeCallbacks = append(t.writeCallbacks, w)
 }
 
+// AddAsyncWriteCallback adds a callback that will be run in a goroutine when t 
+// is written to using SetValue.
 func (t *Trigger) AddAsyncWriteCallback(w WriteCallback) {
 	t.writeCallbacks = append(t.writeCallbacks, makeAsyncWrite(w))
 }
 
+// AddConcurentWriteCallback adds a callback that will run concurrently when
+// t is written to using SetValue.
 func (t *Trigger) AddConcurrentWriteCallback(w WriteCallback) {
 	conWriteLock.Lock()
 		if conWrite == nil {
@@ -90,6 +120,9 @@ func (t *Trigger) AddConcurrentWriteCallback(w WriteCallback) {
 	t.writeCallbacks = append(t.writeCallbacks, makeConcurrentWrite(w))
 }
 
+// AddConditionalWriteCallback adds a callback that will be run  when t is 
+// written to using SetValue only if f evaluates to true when passed the
+// previous and new value of t.
 func (t *Trigger) AddConditionalWriteCallback(w WriteCallback, f func(interface{}, interface{})bool) {
 	t.writeCallbacks = append(t.writeCallbacks, makeConditionalWrite(w, f))
 }
