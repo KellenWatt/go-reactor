ac24ad35dc9d5423e0400e86d8a0cfa45213b952
Kellen Watt <kbw6d9@mst.edu>
2020-04-22 22:32:25 -0500

    Add documentation for Indicator

--- indicator.go
+++ indicator.go
@@ -4,6 +4,9 @@ import (
 	"sync"
 )
 
+// Indicator implements the Binder interface. Indicator provides a mutex, Lock,
+// as a convenience for handling shared resources in asynchronous and 
+// concurrent callbacks.
 type Indicator struct {
 	Lock sync.Mutex
 	value interface{}
@@ -15,6 +18,11 @@ type Indicator struct {
 	delayedBindings []binding // supervisor reactors
 }
 
+// Value returns the value underlying n and runs any callbacks associated with
+// reading. Additionaly, delayed bindings associated with n will be evaluated. 
+// A type assertion will probably be needed to meaningfully use the returned 
+// value. If n has never been set, Value returns nil and any callbacks will be 
+// passed nil.
 func (n *Indicator) Value() interface{} {
 	n.Lock.Lock()
 		v := n.value
@@ -34,6 +42,9 @@ func (n *Indicator) Value() interface{} {
 	return v
 }
 
+// SetValue sets the value underlying n and runs any callbacks associated 
+// with writing. If the current value is nil (for example, if n has not been 
+// set yet), the previous value in callbacks will be nil.
 func (n *Indicator) SetValue(v interface{}) {
 	n.Lock.Lock()
 		prev := n.value
@@ -52,18 +63,32 @@ func (n *Indicator) SetValue(v interface{}) {
 	}
 }
 
+// AddBinder adds a Binder to be executed when the value of n changes. If 
+// concurrent is true, b.SetValue will not be called when n changes; instead it
+// will be queued for change.
+//
+// AddBinder is largely intended for use in implementing Binders, and its 
+// use is heavily discouraged outside of that. If concurrent is true outside 
+// of Binder implementations, f may or may not have the desired effect.
+// If concurrent is false, this will have exactly the same effect as 
+// Binder.AddBinding(), which is the preferred method of creating bindings.
 func (n *Indicator) AddBinder(b Binder, f BindingFunc, concurrent bool) {
 	n.bindings = append(n.bindings, binding{n, b, f, concurrent})
 }
 
+// AddReadCallback adds a callback that will be run when n is read using Value.
 func (n *Indicator) AddReadCallback(r ReadCallback) {
     n.readCallbacks = append(n.readCallbacks, r)
 }
 
+// AddAsyncReadCallback adds a callback that will be run in a goroutine when n
+// is read using Value.
 func (n *Indicator) AddAsyncReadCallback(r ReadCallback) {
     n.readCallbacks = append(n.readCallbacks, makeAsyncRead(r))
 }
 
+// AddConcurrentReadCallback adds a callback that will be run concurrently
+// when n is read using Value.
 func (n *Indicator) AddConcurrentReadCallback(r ReadCallback) {
     conReadLock.Lock()
         if conRead == nil {
@@ -74,18 +99,26 @@ func (n *Indicator) AddConcurrentReadCallback(r ReadCallback) {
     n.readCallbacks = append(n.readCallbacks, makeConcurrentRead(r))
 }
 
+// AddConditionalReadCallback adds a callback that will be run when n is read
+// using Value only if f evaluates to true when passed the value of n.
 func (n *Indicator) AddConditionalReadCallback(r ReadCallback, f func(interface{})bool) {
     n.readCallbacks = append(n.readCallbacks, makeConditionalRead(r, f))
 }
 
+// AddWriteCallback adds a callback that will be run when n is written to 
+// using SetValue.
 func (n *Indicator) AddWriteCallback(w WriteCallback) {
     n.writeCallbacks = append(n.writeCallbacks, w)
 }
 
+// AddAsyncWriteCallback adds a callback that will be run in a goroutine when n
+// is written to using SetValue.
 func (n *Indicator) AddAsyncWriteCallback(w WriteCallback) {
     n.writeCallbacks = append(n.writeCallbacks, makeAsyncWrite(w))
 }
 
+// AddConcurentWriteCallback adds a callback that will run concurrently when
+// n is written to using SetValue.
 func (n *Indicator) AddConcurrentWriteCallback(w WriteCallback) {
     conWriteLock.Lock()
         if conWrite == nil {
@@ -96,24 +129,40 @@ func (n *Indicator) AddConcurrentWriteCallback(w WriteCallback) {
     n.writeCallbacks = append(n.writeCallbacks, makeConcurrentWrite(w))
 }
 
+// AddConditionalWriteCallback adds a callback that will be run when n is
+// written to using SetValue only if f evaluates to true when passed the
+// previous and new value of n.
 func (n *Indicator) AddConditionalWriteCallback(w WriteCallback, f func(interface{}, interface{})bool) {
 	n.writeCallbacks = append(n.writeCallbacks, makeConditionalWrite(w, f))
 }
 
-
+// AddBinding binds n to i, with the value of n being determined by calling f 
+// with the value of i.
 func (n *Indicator) AddBinding(i Initiator, f BindingFunc) {
 	i.AddBinder(n, f, false)
 }
 
+// AddTrivialBinding binds n to i, with the value of n being equal to the value 
+// of i.
+//
+// This is equivalent to calling AddBinding with the function returning the 
+// input value.
 func (n *Indicator) AddTrivialBinding(i Initiator) {
 	i.AddBinder(n, func(v interface{})interface{}{return v}, false)
 }
 
+// AddDelayedBinding binds n to i, but the value of n is only determined when 
+// Value is called. Because of this, f is only called at the last possible 
+// moment. Consequently, this binding behaves differently from the others, 
+// and any side effects will be affected as such.
 func (n *Indicator) AddDelayedBinding(i Initiator, f BindingFunc) {
 	n.delayedBindings = append(n.delayedBindings, binding{i, n, f, false})
 }
 
-
+// AddConcurrentBinding bind n to i, with the value of n being eventually 
+// determined by i. The value passed to f when it is eventually called is
+// the value of i immediately after it triggers the binding, to ensure 
+// consistency.
 func (n *Indicator) AddConcurrentBinding(i Initiator, f BindingFunc) {
 	conBindLock.Lock()
 		if conBind == nil {
