97e408092d19de4d407e1bf7c2ef5ced733525fa
Kellen Watt <kbw6d9@mst.edu>
2020-04-29 15:11:37 -0500

    Add documentation to wrapper functions

--- slice/wrappers.go
+++ slice/wrappers.go
@@ -4,6 +4,9 @@ import (
 	"github.com/KellenWatt/reactor"
 )
 
+// SliceReadCallback takes a function with an []interface{} parameter and wraps 
+// it in a reactor.ReadCallback. This is to simplify writing read callbacks for 
+// Trigger in this package.
 func SliceReadCallback(f func([]interface{})) reactor.ReadCallback {
     return func(v interface{}) {
         val := v.([]interface{})
@@ -11,6 +14,9 @@ func SliceReadCallback(f func([]interface{})) reactor.ReadCallback {
     }
 }
 
+// SliceWriteCallback takes a function with two []interface{} parameters and 
+// wraps it in a reactor.WriteCallback. This is to simplify writing write 
+// callbacks for Trigger in this package.
 func SliceWriteCallback(f func([]interface{}, []interface{})) reactor.WriteCallback {
     return func(prev, v interface{}) {
         prevVal := prev.([]interface{})
@@ -19,7 +25,9 @@ func SliceWriteCallback(f func([]interface{}, []interface{})) reactor.WriteCallb
     }
 }
 
-// Helper methods for creating index callbacks, encapsulates more complex functions
+// IndexReadCallback takes a function with a int and interface{} paramters and 
+// wraps it in a ReadCallback. This function decomposes an Index struct and 
+// passes its members as arguments to f.
 func IndexReadCallback(f func(int,interface{})) reactor.ReadCallback {
     return func(v interface{}) {
         val := v.(Index)
@@ -27,6 +35,10 @@ func IndexReadCallback(f func(int,interface{})) reactor.ReadCallback {
     }
 }
 
+// IndexWriteCallback takes a function with two int and interface{} paramter 
+// pairs and wraps it in a WriteCallback. This function decomposes previous 
+// and new value Index structs and passes their members as arguments to f in 
+// a similar order to standard WriteCallbacks.
 func IndexWriteCallback(f func(int,interface{},int,interface{})) reactor.WriteCallback {
     return func(prev, v interface{}) {
         prevVal := prev.(Index)
