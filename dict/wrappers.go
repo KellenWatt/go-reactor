d1de2300ba104e3931334ffd59879a61dd30b2e6
Kellen Watt <kbw6d9@mst.edu>
2020-05-04 01:19:16 -0500

    Add docs to wrappers

--- dict/wrappers.go
+++ dict/wrappers.go
@@ -4,6 +4,9 @@ import (
 	"github.com/KellenWatt/reactor"
 )
 
+// MapReadCallback takes a function with an map[interface{}]interface{} 
+// parameter and wraps it in a reactor.ReadCallback. This is to simplify 
+// writing read callbacks for Trigger in this package.
 func MapReadCallback(f func(map[interface{}]interface{})) reactor.ReadCallback {
 	return func(v interface{}) {
 		val := v.(map[interface{}]interface{})
@@ -11,6 +14,9 @@ func MapReadCallback(f func(map[interface{}]interface{})) reactor.ReadCallback {
 	}
 }
 
+// MapWriteCallback takes a function with two map[interface{}]interface{} 
+// parameters and wraps it in a reactor.WriteCallback. This is to simplify 
+// writing write callbacks for Trigger in this package.
 func MapWriteCallback(f func(map[interface{}]interface{}, map[interface{}]interface{})) reactor.WriteCallback {
 	return func(prev, v interface{}) {
 		p := prev.(map[interface{}]interface{})
@@ -19,6 +25,9 @@ func MapWriteCallback(f func(map[interface{}]interface{}, map[interface{}]interf
 	}
 }
 
+// KeyReadCallback takes a function with two interface{} paramters and wraps 
+// it in a ReadCallback. This function decomposes a Pair struct and passes its 
+// members as arguments to f.
 func KeyReadCallback(f func(interface{}, interface{})) reactor.ReadCallback {
 	return func(v interface{}) {
 		val := v.(Pair)
@@ -26,6 +35,10 @@ func KeyReadCallback(f func(interface{}, interface{})) reactor.ReadCallback {
 	}
 }
 
+// KeyWriteCallback takes a function with two sets of interface{} paramter 
+// pairs and wraps it in a WriteCallback. This function decomposes previous 
+// and new value Pair structs and passes their members as arguments to f in 
+// a similar order to standard WriteCallbacks.
 func KeyWriteCallback(f func(interface{}, interface{}, interface{}, interface{})) reactor.WriteCallback {
 	return func(prev, v interface{}) {
 		p := prev.(Pair)
